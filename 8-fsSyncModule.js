e6c8047911895164f11c14097e06a7729b054e0a
Pranav Kulkarni <pranav.kulkarni@sap.com>
2023-03-08 10:42:01 +0530

    Streams-http example

--- 8-fsSyncModule.js
+++ 8-fsSyncModule.js
@@ -2,15 +2,11 @@ const { readFileSync, writeFileSync } = require("fs");
 console.log("Start");
 const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
-
-console.log(first, second);
-
 //creates a file if not present and overwrites the data
 writeFileSync(
   "./content/result.txt",
   `Here is the result : ${first} ${second}`
 );
-
 //Append the data present in file
 writeFileSync(
   "./content/result.txt",
