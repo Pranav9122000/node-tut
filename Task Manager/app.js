0875b1e35ca75bf6012b4b96c6f9daccd684406c
Pranav Kulkarni <pranav.kulkarni@sap.com>
2023-03-24 13:28:36 +0530

    New

--- Task Manager/app.js
+++ Task Manager/app.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
-
+const connectDB = require("./db/connect");
+require("dotenv").config();
 //middleware
 app.use(express.json());
 
-
 //routes
 app.get("/hello", (req, res) => {
-    res.send("Task Manager App ")
+  res.send("Task Manager App ");
 });
 
 app.use("/api/v1/tasks", tasks);
-
 
 // app.get("/api/v1/tasks")        -   get all the tasks
 // app.post("/api/v1/tasks")       -   create a new tasks
@@ -22,5 +21,13 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 4000;
 
-app.listen(port, console.log(`Server is listening on port ${port}......`))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}......`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+start();
